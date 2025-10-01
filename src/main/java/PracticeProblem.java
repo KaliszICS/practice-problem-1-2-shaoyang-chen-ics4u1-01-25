b56486255986fb8856b1cff854c2feeac2bf8408
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-10-01 13:23:32 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,38 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean validIndex(int[] arr1, int index) {
+		int length = arr1.length;
+		try {
+            arr1[index] = 11451419;
 
+			if (arr1[index] == 11451419) {
+				return true;
+			}
+
+        }catch (ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+		return false;
+	}
+	public static int divide(int a, int b) {
+		int result = 0;
+		try {
+			result = a / b;
+			return result;
+		} catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+	public static int saveConvertStringtoInt(String input) {
+		int result = 0;
+		try{
+			result = Integer.parseInt(input);
+			return result;
+		} catch (NumberFormatException e){
+			return 0;
+		}
+	}
 	
 
 }
